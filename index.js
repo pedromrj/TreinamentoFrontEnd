b3726534c2ac9dbd8957d4817f9f29a246953daa
Pedro Henrique <pedromrj09@gmail.com>
2019-07-14 00:48:55 -0300

    alterações na label cadastro, botão, e faixa de preço na pagina loja

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ function validacao() {
     var dados = document.getElementsByTagName("input");
     var senha1 = dados[3];
     var senha2 = dados[4];
+
+    for (let index = 0; index < dados.length; index++) {
+        if(!dados[index]) {
+            document.getElementsByName("senha").style.border="solid 1px red";    
+        }
+    }
     
     if (senha1 != senha2) {
         document.getElementById("error").innerHTML = "preencher todos os campo para concluir!";
