362ee6375fd20c677ed80704c6cc132f6b11a575
Pedro Henrique <pedromrj09@gmail.com>
2019-07-15 01:19:56 -0300

    algumas alterações na posição do preço e js

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function validacao() {
-    var dados = document.getElementsByTagName("input");
-    var senha1 = dados[3];
-    var senha2 = dados[4];
+    var dados = document.getElementsByClassName("fields");
+    var senha1 = dados[3].value;
+    var senha2 = dados[4].value;
+    var vazio = false;
 
-    for (let index = 0; index < dados.length; index++) {
-        if(!dados[index]) {
-            document.getElementsByName("senha").style.border="solid 1px red";    
+    for (let i = 0; i < dados.length; i++) {
+        if (!dados[i]) {
+            document.getElementsById("error").innerHTML = "preencher todos os campo para concluir!";
+            document.getElementsById(dados[i].getElementsById).style.border="1px solid red";
+            vazio = true;
         }
     }
+
+    if (vazio == false) {
+        if (senha1.lenght < 5 || senha2.lenght < 5) {
+            document.getElementsById("senha").style.border="1px solid red";
+            document.getElementsById("re-senha").style.border="1px solid red";
+            document.getElementsById("error").innerHTML = "Tamanho maximo 5";
+            return false;
+        }
     
-    if (senha1 != senha2) {
-        document.getElementById("error").innerHTML = "preencher todos os campo para concluir!";
-        document.getElementsByName("senha").style.border="solid 1px red";
-        document.getElementsByName("resenha").style.border="solid 1px red";
+        if (senha1 != senha2) {
+            document.getElementsById("error").innerHTML = "preencher todos os campo para concluir!";
+            document.getElementsById("senha").style.border="1px solid red";
+            document.getElementsById("re-senha").style.border="1px solid red";
+            return false;
+        }
     }
 }
