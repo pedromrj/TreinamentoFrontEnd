b1baf7eb514301b74441c2cf53ea3878cc20e13b
Pedro Henrique <pedromrj09@gmail.com>
2019-07-17 00:39:40 -0300

    corrigindo erro no JS

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
 function validacao() {
-    var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
-    var senha = document.getElementById("senha").value;
-    var re_senha = document.getElementById("re-senha").value;
-    var cpf = document.getElementById("cpf").value;
+    var dados = document.getElementsByTagName("input");
     var errors = 0;
-    
-    if (!nome) {
+    const msg = "preencher todos os campos corretamente!!";
+
+    if (!dados[0].value) {
         document.getElementById("nome").style.border="1px solid red";
         errors++;
     }
-    if (!email) {
+    if (!dados[1].value) {
         document.getElementById("email").style.border="1px solid red";
         errors++;
     }
-    if (!cpf) {
+    if (!dados[2].value) {
         document.getElementById("cpf").style.border="1px solid red";
         errors++;
     }
-    if (!senha) {
+    if (!dados[3].value) {
         document.getElementById("senha").style.border="1px solid red";
         errors++;
     }
-    if (!re_senha) {
+    if (!dados[4].value) {
         document.getElementById("re-senha").style.border="1px solid red";
         errors++;
     }
-    if (senha != re_senha) {
+    if (dados[4].value != dados[3].value) {
         document.getElementById("senha").style.border="1px solid red";
         document.getElementById("re-senha").style.border="1px solid red";
         errors++;
     }
-
-    if (senha.lenght < 5 || re-senha.lenght < 5) {
+    
+    if (dados[4].value.length < 5 || dados[3].value.length < 5) {
         document.getElementById("senha").style.border="1px solid red";
-        document.getElementById("re-senha").style.border="1px solid red"
+        document.getElementById("re-senha").style.border="1px solid red";
         errors++;
     }
 
     if (errors > 0) {
-        document.getElementById("msg").innerHTML = "preencher todos os campos corretamente!!";
+        document.getElementById("msg").innerHTML = msg;
     }    
 }
